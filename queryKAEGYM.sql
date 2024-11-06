1b2f12f85e0e5f4fbb4348c1ac849d825b9b3a7a
Andoni <159487811+apastraher@users.noreply.github.com>
2024-11-06 12:08:02 +0100

    Update queryKAEGYM.sql

--- queryKAEGYM.sql
+++ queryKAEGYM.sql
@@ -2,7 +2,7 @@ USE equipod;
 
 -- Tabla Usuarios
 CREATE TABLE Usuarios (
-    id_usuario INT IDENTITY(1,1) PRIMARY KEY,
+    id_usuario INT IDENTITY(1,1) PRIMARY KEY, -- IDENTITY(1,1) es igual que el auto_increment
     nombre VARCHAR(100) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
     telefono VARCHAR(15),
@@ -13,14 +13,14 @@ CREATE TABLE Usuarios (
     tipo_usuario VARCHAR(20) NOT NULL CHECK (tipo_usuario IN ('Cliente', 'Monitor', 'Administrador'))
 );
 
--- Tabla Clientes
+-- Tabla Clientes 
 CREATE TABLE Clientes (
     id_usuario INT PRIMARY KEY,
     ccc VARCHAR(24) NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE
 );
 
--- Tabla Monitores
+-- Tabla Monitores 
 CREATE TABLE Monitores (
     id_usuario INT PRIMARY KEY,
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE
@@ -32,12 +32,12 @@ CREATE TABLE Administradores (
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE
 );
 
--- Tabla Actividades
+-- Tabla Actividades 
 CREATE TABLE Actividades (
     id_actividad INT IDENTITY(1,1) PRIMARY KEY,
     nombre_actividad VARCHAR(100) NOT NULL UNIQUE,
     descripcion VARCHAR(100) NOT NULL,
-    id_monitor INT NOT NULL,
+    id_monitor INT NULL,
     FOREIGN KEY (id_monitor) REFERENCES Monitores(id_usuario) ON DELETE SET NULL
 );
 
