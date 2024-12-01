1a725070699c301df2bdcc175178048cfcb5b513
apastraher <apastraher@gmail.com>
2024-12-01 16:47:55 +0100

    Tests creados

---
+++ KAETesting/ApuntarDesapuntarActividades.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Negocio.EntitiesDTO;
+using Negocio.Management;
+using System;
+
+namespace KAETesting
+{
+    [TestClass]
+    public class ApuntarDesapuntarActividades
+    {
+        private ActividadManagement actividadManagement;
+        private UsuarioManagement usuarioManagement;
+        private UsuariosDTO usuario;
+        private ActividadDTO actividad;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Instanciar las capas de negocio y objetos necesarios
+            actividadManagement = new ActividadManagement();
+            usuarioManagement = new UsuarioManagement();
+        }
+
+        [TestMethod]
+        public void CP1_1_1_ApuntarActividad()
+        {
+            bool usuarioApuntado = usuarioManagement.apuntarUsuarioActividad("apastraher@gmail.com", "Zumba");
+
+            Assert.IsTrue(usuarioApuntado, "El usuario ha sido apuntado exitosamente");
+        }
+
+        [TestMethod]
+        public void CP2_1_1_DesapuntarActividad()
+        {
+            // Suponer que el usuario ya está apuntado
+            bool desapuntarUsuario = usuarioManagement.desapuntarUsuarioActividad("apastraher@gmail.com", "Zumba");
+
+            // Verificar que el resultado sea positivo
+            Assert.IsTrue(desapuntarUsuario, "El usuario no pudo desapuntarse de la actividad.");
+        }
+    }
+}
