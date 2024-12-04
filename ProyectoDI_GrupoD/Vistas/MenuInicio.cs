b51f91549914ef3b8c6a8420dbe8b1420d146cfd
apastraher <apastraher@gmail.com>
2024-12-04 17:43:16 +0100

    Empiezo cambio de diseño.
    Aun falta por redondear el textbox, dar color y venderlo

--- ProyectoDI_GrupoD/Vistas/MenuInicio.cs
+++ ProyectoDI_GrupoD/Vistas/MenuInicio.cs
@@ -46,5 +46,15 @@ namespace ProyectoDI_GrupoD.Vistas
         {
             Application.Exit();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
     }
 }
