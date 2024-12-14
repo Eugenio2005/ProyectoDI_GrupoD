b65cab8dd71ff6b194cfc763a417c7b31478df0e
Eugenio2005 <eugenio.locris@gmail.com>
2024-12-14 20:50:19 +0100

    cambios para funcionamiento de admin

--- Negocio/Management/UsuarioManagement.cs
+++ Negocio/Management/UsuarioManagement.cs
@@ -64,7 +64,7 @@ namespace Negocio.Management
                 }
 
                 // Obtener el cliente asociado de la base de datos según el ID
-                Clientes clienteBD = new Datos.Repositories.ClientRepository().ConsultarClienteDNI(usuarioDTO.Dni);
+                Clientes clienteBD = new Datos.Repositories.ClientRepository().ConsultarClienteDNI(usuarioDTO.Email);
                 if (clienteBD == null)
                 {
                     throw new Exception("No se encontró un cliente con el email proporcionado.");
@@ -82,8 +82,6 @@ namespace Negocio.Management
                 clienteBD.ccc = clienteDTO.CuentaCorriente;
                 clienteBD.email = clienteDTO.Email;
 
-
-                //bool usuarioActualizado = new Datos.Repositories.ClientRepository().ActualizarCliente(usuarioBD, clienteBD);
                 new Datos.Repositories.ClientRepository().ActualizarCliente(usuarioBD, clienteBD);
 
                 // Cargar los datos del usuario en DatosUsuario
@@ -100,8 +98,8 @@ namespace Negocio.Management
                 };
 
                 DatosUsuario.SetDatosUsuario(usuarioDTO);
-                    return true;
-                }
+                return true;
+            }
             catch (Exception ex)
             {
                 // Manejo de errores
