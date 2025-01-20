b0d70ade74684e8c524be13226b7a8be88818140
apastraher <159487811+apastraher@users.noreply.github.com>
2025-01-20 19:32:00 +0100

    Arreglo error botón actualizar contraseña
    Antes el botón no estaba deshabilitado si los campos no estaban rellenos.

--- ProyectoDI_GrupoD/Vistas/CambiarContraseña.cs
+++ ProyectoDI_GrupoD/Vistas/CambiarContraseña.cs
@@ -20,6 +20,7 @@ namespace ProyectoDI_GrupoD.Vistas
         {
             InitializeComponent();
             aplicarEventoComprobarTextBox();
+            btnRegistrarRe.Enabled = false;
         }
 
         /// <summary>
